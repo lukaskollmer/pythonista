d8942d16eea833116fff2b482ce1ca8778bdb143
Lukas Kollmer <lukas@kollmer.me>
2016-08-12 15:43:04 +0200

    added theme.py

---
+++ theme.py
@@ -0,0 +1,19 @@
+import os
+import editor
+
+
+def custom_themes():
+	home = os.getenv('CFFIXED_USER_HOME')
+	user_themes_path = os.path.join(home, 'Library')
+	
+	print(os.listdir(user_themes_path))
+
+def get():
+	return editor.get_theme_dict()
+	
+def background_color():
+	return get().get("background")
+
+if __name__ == "__main__":
+	print(get())
+	print(background_color())
